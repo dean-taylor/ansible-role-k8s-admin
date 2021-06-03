d9f9347db631b4195aa4303129e0f69ac268d61d
Dean Taylor <dean@gohitech.net>
2021-06-03 08:36:43 +0800

    fix kustomize helm temp file location, must be in CWD or child folder

--- files/kustomize-wrapper.sh
+++ files/kustomize-wrapper.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# /usr/local/bin/kustomize-wrapper.sh
+# ensure file is executable
 # helm install CHART REPO/CHART --post-renderer kustomize-wrapper.sh
 # function helm () { HELM=`which helm`; [[ $1 =~ ^install|upgrade$ ]] && $HELM $@ --post-renderer kustomize-wrapper.sh || $HELM $@; }
 
@@ -8,7 +10,7 @@ function cleanup () {
 }
 
 # Create temporary file for processing helm output
-TMPFILE=$(mktemp /tmp/kustomize.XXXXXX)
+TMPFILE=$(mktemp kustomize.XXXXXX)
 # On script exit clean up workspace
 trap cleanup EXIT
 
@@ -17,6 +19,7 @@ trap cleanup EXIT
 
 # Stream stdin to temporary file for kustomize processing
 cat <&0 >"${TMPFILE}"
-
+# Add a temporary resource entry for the Helm resource definitions
 kustomize edit add resource "${TMPFILE}"
+# Run the Kustomize build operation
 kustomize build .
